81a28ca7bd4f323050a7daa6da5e713d6aeceb4f
NataliaIvasiv <143248403+NataliaIvasiv@users.noreply.github.com>
2024-08-16 11:32:45 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favourite" element={<About />} />
+        <Route path="/catalog" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
